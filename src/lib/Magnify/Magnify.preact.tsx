bb07baed0062f4b34d220c0a569d060a74459aef
Andrew Hoang <hoangaatrox23504@gmail.com>
2026-08-14 20:18:11 +0700

    fix(magnify): include sync store dependencies

--- src/lib/Magnify/Magnify.preact.tsx
+++ src/lib/Magnify/Magnify.preact.tsx
@@ -19,7 +19,7 @@ function useSyncExternalStore<T>(
   useEffect(() => {
     const unsubscribe = subscribe(() => setValue(getSnapshot()));
     return unsubscribe;
-  }, []);
+  }, [subscribe, getSnapshot]);
   return value;
 }
 
