00fd4667cae04f043bd17c2a5a194f4d075f8510
Andrew Hoang <hoangaatrox23504@gmail.com>
2026-08-14 20:18:10 +0700

    fix(grid): include sync store dependencies

--- src/lib/Grid/Grid.preact.tsx
+++ src/lib/Grid/Grid.preact.tsx
@@ -18,7 +18,7 @@ function useSyncExternalStore<T>(
   useEffect(() => {
     const unsubscribe = subscribe(() => setValue(getSnapshot()));
     return unsubscribe;
-  }, []);
+  }, [subscribe, getSnapshot]);
   return value;
 }
 
